1428e15d6aed5f9fdb77a5ab865e43acc0b0eda9
Anant Kanchan <anant4265@gmail.com>
2022-01-27 20:36:43 +0530

    :tada: initial commit

---
+++ topmap/topmapApp.swift
@@ -0,0 +1,17 @@
+//
+//  topmapApp.swift
+//  topmap
+//
+//  Created by Anant Kanchan on 27/01/22.
+//
+
+import SwiftUI
+
+@main
+struct topmapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
