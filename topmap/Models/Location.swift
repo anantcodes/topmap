4fdc5c718640fccb556d9320a74633b6434966d1
Anant Kanchan <anant4265@gmail.com>
2022-01-29 14:32:02 +0530

    :tada: static data entry setup

---
+++ topmap/Models/Location.swift
@@ -0,0 +1,18 @@
+//
+//  Location.swift
+//  topmap
+//
+//  Created by Anant Kanchan on 29/01/22.
+//
+
+import Foundation
+import MapKit
+
+struct  Location {
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+}
